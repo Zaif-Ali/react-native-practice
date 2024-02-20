1bc1a8108fb48457a17cf2e1de43909bc62cb562
ZAIF ALI <zaifali785@gmail.com>
2024-02-21 00:42:20 +0500

    employee cards

---
+++ constants/contactData.ts
@@ -0,0 +1,56 @@
+export const EmployeeData = [
+  {
+    name: 'John Doe',
+    gender: 'male',
+    imageUrl: 'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    uuid: '1234567890',
+  },
+  {
+    name: 'Jane Smith',
+    gender: 'female',
+    imageUrl: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    uuid: '0987654321',
+  },
+  {
+    name: 'Michael Johnson',
+    gender: 'male',
+    imageUrl: 'https://images.pexels.com/photos/13026079/pexels-photo-13026079.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    uuid: '2468135790',
+  },
+  {
+    name: 'Emily Davis',
+    gender: 'female',
+    imageUrl: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    uuid: '1357924680',
+  },
+  {
+    name: 'Sarah Wilson',
+    gender: 'female',
+    imageUrl: 'https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    uuid: '0123456789',
+  },
+  {
+    name: 'Christopher Taylor',
+    gender: 'male',
+    imageUrl: 'https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    uuid: '8642097531',
+  },
+  {
+    name: 'Olivia Martinez',
+    gender: 'female',
+    imageUrl: 'https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    uuid: '7531902468',
+  },
+  {
+    name: 'Daniel Anderson',
+    gender: 'male',
+    imageUrl: 'https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    uuid: '5678901234',
+  },
+  {
+    name: 'Sophia Thomas',
+    gender: 'female',
+    imageUrl: 'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    uuid: '4321098765',
+  },
+]
