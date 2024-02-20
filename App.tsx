1b34cca5e4cef7ec1bf6fc26d834a6804936f00c
ZAIF ALI <zaifali785@gmail.com>
2024-02-21 00:12:58 +0500

    changes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import {SafeAreaView, ScrollView} from 'react-native'
 import React from 'react'
 import FlatCards from './components/FlatCards'
-import ContactList from './components/ContactList'
 
 export default function App() {
   return (
     <SafeAreaView>
       <ScrollView>
-        {/* <FlatCards/> */}
-        <ContactList/>
+        <FlatCards/>
       </ScrollView>
     </SafeAreaView>
   )
